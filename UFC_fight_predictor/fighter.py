6f01bc796708e534bc6bd7bc28799956a0f95390
Lannan96 <32526874+Lannan96@users.noreply.github.com>
2024-02-07 19:21:39 +0000

    Added previous python files

---
+++ UFC_fight_predictor/fighter.py
@@ -0,0 +1,15 @@
+class Fighter:
+
+    def __init__(self, name, nickname):
+        self.name = name
+        self.name = nickname
+        self.height = height
+        self.weight = weight
+        self.reach = reach
+        self.stance = stance
+        self.DOB = DOB
+        self.wins = wins
+        self.losses = losses
+        self.draws = draws
+        self.noContests = noContests
+        self.slpm = slpm
---
+++ UFC_fight_predictor/mySQLConnector.py
@@ -0,0 +1,41 @@
+
+import mysql.connector
+
+
+# write a function in the database object that either connects to the database or creates once if it doesn't exist
+
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Ashtead96',
+    database='ufc_database'
+)
+
+mycursor = db.cursor()
+
+#mycursor.execute('CREATE DATABASE ufc_database')
+
+# create table
+
+mycursor.execute(('CREATE TABLE Fighter (fighterID int PRIMARY KEY AUTO_INCREMENT)'))
+mycursor.execute('DESCRIBE Fighter')
+for x in mycursor:
+    print(x)
+
+
+# create fight table
+
+
+# function to insert values into a table
+def insert_fighters(fighters_list):
+    for fighter in fighters_list:
+        mycursor.execute('INSERT INTO FIGHTER (name, age) VALUES (%s, %s)', (fighter[0], fighter[1]))
+
+
+
+def retrieve_fighters():
+    mycursor.execute('SELECT * FROM Fighter')
+
+'''
+
+'''
